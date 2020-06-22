0b71a3ea575a76916ad6d0764f5e7dca6d98b0f3
Sudhanshu Ghinmine <sghinmine54@gmail.com>
2020-06-22 23:55:18 +0530

    REFACTORED to use optional arguments instead of arrays and ADDED condition for no arguments passed

--- src/main/java/com/su481ns8u/maximum/FindMaximum.java
+++ src/main/java/com/su481ns8u/maximum/FindMaximum.java
@@ -3,7 +3,7 @@ package com.su481ns8u.maximum;
 import com.su481ns8u.maximum.services.MaxFinder;
 
 public class FindMaximum {
-    public static void main(String[] args) {
+    public static void main(String... args) {
         new MaxFinder(2, 10, 5, 6).findMax();
         new MaxFinder(15.0f, 5.2f, 8.3f, 9.1f).findMax();
         new MaxFinder("Big", "Sudhanshu", "Snehal", "Shashank").findMax();
--- src/main/java/com/su481ns8u/maximum/services/MaxFinder.java
+++ src/main/java/com/su481ns8u/maximum/services/MaxFinder.java
@@ -2,13 +2,20 @@ package com.su481ns8u.maximum.services;
 
 import java.util.Arrays;
 
+/**
+ * Class extends generic Comparable
+ * It finds and prints the max element
+ * @param <E>
+ */
 public class MaxFinder<E extends Comparable> {
     private E[] array;
 
+    /* Constructor */
     public MaxFinder(E... args) {
         this.array = args;
     }
 
+    /* Find and return max */
     public E findMax() {
         if (array.length <= 0) {
             System.out.println("No Arguments Passed !");
@@ -21,6 +28,7 @@ public class MaxFinder<E extends Comparable> {
         }
     }
 
+    /* Prints max of all arguments */
     private void printMax(E max) {
         System.out.println("Max Element: " + max);
     }
